73f8dd4acd83268dfd6c0e9bb096d8e56e2954c2
Lindsay WIllmore <willmore@scotty.pni.Princeton.EDU>
2022-03-30 11:53:15 -0400

    Notebooks for extracting features from tracking data and performing supervised and unsupervised behavior characterization

---
+++ QuantifyBehavior/behavior_utils.py
@@ -0,0 +1,33 @@
+## define your feature names, used across all behavior analysis
+
+FEATURE_NAMES = [
+"Proximity", 
+"swNose-blTTI dist", 
+"swNose-blNOSE dist", 
+"SW velocity", 
+"BL velocity", 
+"SW side height", 
+"BL side height",
+"SW orientaiton",
+"BL orientation",
+"BL-SW between centroid velocity",
+"BL dist to short wall",
+"BL dist to long wall"
+]
+
+FEAT_RANGES=['Far                     Close',
+           'Far                     Close',
+           'Far                     Close',
+           'Fast                Slow',
+           'Fast                     Slow',
+           'Low                     High',
+           'Low                     High',
+           'Toward             Away',
+           'Toward             Away',
+           'Fast                Slow',
+           'Far                     Close',
+           'Far                     Close'
+          ]
+
+VID_FR = 100
+
